beea71dee9c9753d74a6f48f4ad59a6a11c0720a
Spark4444 <102835794+Spark4444@users.noreply.github.com>
2025-08-30 16:34:53 +0200

    Updated imports

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,20 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stringToDataURI = stringToDataURI;
-exports.default = fetchFile;
-const path_1 = __importDefault(require("path"));
-const mime_1 = __importDefault(require("mime"));
-const puppeteer_1 = __importDefault(require("puppeteer"));
+import path from "path";
+import mime from "mime";
+import puppeteer from "puppeteer";
 // Convert a file to base64 data URI
-function stringToDataURI(content, fileName) {
+export function stringToDataURI(content, fileName) {
     // Get the MIME type of the file
-    const mimeType = mime_1.default.getType(fileName) || "application/octet-stream";
+    const mimeType = mime.getType(fileName) || "application/octet-stream";
     // Convert the content to base64
     const base64Data = content.toString("base64");
     // Return the data URI
     return `data:${mimeType};base64,${base64Data}`;
 }
 // Function to download a file from a URL and return its content as a string or base64 data URI
-async function fetchFile(url) {
+export default async function fetchFile(url) {
     var _a;
     // Launch puppeteer and use page.evaluate to fetch the file
-    const browser = await puppeteer_1.default.launch();
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     try {
         // Use fetch API within the browser context to download the file
@@ -44,8 +37,8 @@ async function fetchFile(url) {
         // Get MIME type from response headers
         const contentType = ((_a = responseHeaders["content-type"]) === null || _a === void 0 ? void 0 : _a.split(";")[0]) || "";
         // Get the file name and extension from the URL
-        const fileName = path_1.default.basename(url.split("?")[0]);
-        const fileExtension = path_1.default.extname(fileName);
+        const fileName = path.basename(url.split("?")[0]);
+        const fileExtension = path.extname(fileName);
         let output = "";
         // Use content type from headers if no extension, otherwise fall back to extension check
         const isTextBased = contentType.includes("text/") ||
